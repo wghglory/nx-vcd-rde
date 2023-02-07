835557d51f7880f985b1af1e5c917f42c8bbcc30
Derek Wang <guanghuiw@vmware.com>
2023-02-07 09:54:54 +0800

    feat: new-article workspace generator

---
+++ tools/generators/new-article/index.ts
@@ -0,0 +1,38 @@
+import { Tree, formatFiles, installPackagesTask, names, generateFiles, joinPathFragments } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+
+interface NewArticleSchemaOptions {
+  title: string;
+  author: string;
+  excerpt?: string;
+}
+
+export default async function (tree: Tree, schema: NewArticleSchemaOptions) {
+  // await libraryGenerator(tree, { name: schema.name });
+
+  generateFiles(
+    // virtual file system
+    tree,
+
+    // the location where the template files are
+    joinPathFragments(__dirname, './files'),
+
+    // where the files should be generated
+    './_articles',
+
+    // the variables to be substituted in the template
+    {
+      title: schema.title,
+      author: schema.author,
+      excerpt: schema.excerpt || '',
+      normalizedTitle: names(schema.title).fileName,
+      creationDate: new Date().toISOString(),
+    },
+  );
+
+  await formatFiles(tree);
+
+  // return () => {
+  //   installPackagesTask(tree);
+  // };
+}
