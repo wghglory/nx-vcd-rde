b7a8b5dcde6ea99163819a7c116752a2e011b1a9
Derek Wang <guanghuiw@vmware.com>
2023-02-09 18:38:27 +0800

    test: product state

--- libs/feature/product/src/lib/services/product-state.service.spec.ts
+++ libs/feature/product/src/lib/services/product-state.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 
+import { Product } from '../models/product';
 import { ProductStateService } from './product-state.service';
 
 describe('ProductStateService', () => {
@@ -12,5 +14,24 @@ describe('ProductStateService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('refresh list', () => {
+    service.refreshList();
+
+    expect(subscribeSpyTo(service.refreshAction$).getFirstValue()).toBeUndefined();
+  });
+
+  it('select item', () => {
+    const product = {
+      id: 'mockId',
+      name: 'mock product',
+      description: 'description',
+      productionDate: new Date().toISOString(),
+    } as Product;
+
+    service.selectItem(product);
+
+    expect(subscribeSpyTo(service.selectedItem$).getFirstValue()).toEqual(product);
   });
 });
