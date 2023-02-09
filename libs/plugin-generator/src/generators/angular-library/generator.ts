c22ea14df0c79537ece13c5f5fdc5d7328050544
Derek Wang <guanghuiw@vmware.com>
2023-02-09 16:37:56 +0800

    test: generator jest template

--- libs/plugin-generator/src/generators/angular-library/generator.ts
+++ libs/plugin-generator/src/generators/angular-library/generator.ts
@@ -1,5 +1,15 @@
 import { libraryGenerator } from '@nrwl/angular/generators';
-import { formatFiles, generateFiles, getWorkspaceLayout, joinPathFragments, names, Tree } from '@nrwl/devkit';
+import {
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  installPackagesTask,
+  joinPathFragments,
+  names,
+  offsetFromRoot,
+  readProjectConfiguration,
+  Tree,
+} from '@nrwl/devkit';
 
 import { AngularLibraryGeneratorSchema } from './schema';
 
@@ -8,17 +18,35 @@ export default async function (tree: Tree, options: AngularLibraryGeneratorSchem
   const name = names(options.name).fileName;
   const projectDirectory = options.directory ? `${names(options.directory).fileName}/${name}` : name;
   const projectRoot = joinPathFragments(libsDir, projectDirectory);
+  const libraryName = options.directory ? `${options.directory}-${options.name}` : options.name;
+
   const tags = `project:${options.domain},scope:${options.scope},type:${options.type},framework:angular`;
+
+  // console.log(libsDir); // libs
+  // console.log(name); // book
+  // console.log(projectDirectory); // provider/book
+  // console.log(projectRoot); // libs/provider/book
+  // console.log(libraryName); // provider-book
 
   // run the original generator
   await libraryGenerator(tree, { ...options, tags });
+
+  // const libraryRoot = readProjectConfiguration(tree, libraryName);
+
+  // https://nx.dev/packages/devkit/documents/index#offsetfromroot
+  const relativeOffset = offsetFromRoot(projectRoot); // ../../../
 
   // Add own custom files
   generateFiles(
     tree, // the virtual file system
     joinPathFragments(__dirname, './files'), // path to the file templates)
     projectRoot, // destination path of the files
-    { ...options, ...names(options.name), template: '' }, // config object to replace variable in file templates
+    { ...options, projectRoot, libraryName, relativeOffset, ...names(options.name), template: '' }, // config object to replace variable in file templates
   );
+
   await formatFiles(tree);
+
+  return () => {
+    installPackagesTask(tree);
+  };
 }
