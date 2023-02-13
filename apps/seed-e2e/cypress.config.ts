c3a9033376d82a8df56a2e5a5b60422c1c7a6816
Derek Wang <guanghuiw@vmware.com>
2023-02-13 22:38:48 +0800

    chore: setup cypress report

--- apps/seed-e2e/cypress.config.ts
+++ apps/seed-e2e/cypress.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'twdu2f', // for cypress cloud
   e2e: nxE2EPreset(__dirname),
+  reporter: '../../node_modules/mochawesome',
+  reporterOptions: {
+    reportDir: '../../reports/mochawesome-report',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
 });
