f6cedd19c95049470f403ef0a950724937cad8ce
Derek Wang <guanghuiw@vmware.com>
2023-02-19 21:43:43 +0800

    refactor: alert export

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,6 +1,4 @@
-export * as alertActions from './lib/alert/+state/alerts.actions';
-export { Alert } from './lib/alert/+state/alerts.models';
-export * from './lib/alert/alert.module';
+export * from './lib/alert/';
 export * from './lib/loading-or-error/loading-or-error.component';
 export * from './lib/spinner/spinner.component';
 export * from './lib/welcome/welcome.component';
---
+++ libs/shared/ui/src/lib/alert/index.ts
@@ -0,0 +1,3 @@
+export * as alertActions from './+state/alerts.actions';
+export { Alert } from './+state/alerts.models';
+export * from './alert.module';
