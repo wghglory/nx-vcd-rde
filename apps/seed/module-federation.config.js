8cec134733c4a7589e062e07337a77732cdfd265
Derek Wang <guanghuiw@vmware.com>
2023-02-27 21:51:30 +0800

    feat: dynamic module federation https://nx.dev/recipes/module-federation/dynamic-module-federation-with-angular#creating-a-new-dynamic-host-application

--- apps/seed/module-federation.config.js
+++ apps/seed/module-federation.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   name: 'seed',
-  remotes: ['shop-mfe'],
+  // https://nx.dev/recipes/module-federation/dynamic-module-federation-with-angular#creating-a-new-dynamic-host-application
+  // remotes are not required due to dynamic module federation
+  remotes: [],
 };
