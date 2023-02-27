02f7dedd65511c91dfd481d28db819416a9cc136
Derek Wang <guanghuiw@vmware.com>
2023-02-27 16:26:21 +0800

    feature: add mfe-container

--- libs/feature/mfe/src/lib/ui/mfe-container/mfe-container.component.ts
+++ libs/feature/mfe/src/lib/ui/mfe-container/mfe-container.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'seed-mfe-container',
   templateUrl: './mfe-container.component.html',
   styleUrls: ['./mfe-container.component.scss'],
 })
-export class MfeContainerComponent {}
+export class MfeContainerComponent {
+  // TODO: load this from a json File. a generator will update this json
+  apps$ = of([{ id: 1, name: 'First App', description: 'First remote app', url: 'http://localhost:4301' }]);
+}
