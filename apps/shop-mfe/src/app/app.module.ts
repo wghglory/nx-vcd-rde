d6aca03fbd83b3cd3110bc4988c150eca47c1539
Derek Wang <guanghuiw@vmware.com>
2023-02-27 23:13:03 +0800

    feat(mfe): host and remote apps shares libraries, theme for remote apps

--- apps/shop-mfe/src/app/app.module.ts
+++ apps/shop-mfe/src/app/app.module.ts
@@ -1,13 +1,69 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NavbarComponent, ToastModule, VerticalNavComponent } from '@seed/core/ui';
+import { AlertModule } from '@seed/shared/ui';
+import { VmwClarityThemeConfig, VmwClarityThemeService, VmwThemeToolsModule } from '@vmw/ngx-utils';
+import { VIPModule } from '@vmw/ngx-vip';
 
 import { AppComponent } from './app.component';
+
+export function themeLoader(themeService: VmwClarityThemeService) {
+  return () => {
+    return new Promise<void>(resolve => {
+      themeService
+        .initialize({
+          clarityDarkPath: '/clr-ui-dark.min.css',
+          clarityLightPath: '/clr-ui.min.css',
+        } as VmwClarityThemeConfig)
+        .then(() => {
+          resolve();
+        });
+    });
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        },
+      },
+    ),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+    ClarityModule,
+    VIPModule.forRoot(),
+    VmwThemeToolsModule.forRoot(),
+
+    // core
+    VerticalNavComponent,
+    NavbarComponent,
+    ToastModule,
+
+    // share
+    AlertModule,
+
     RouterModule.forRoot(
       [
         {
@@ -18,7 +74,14 @@ import { AppComponent } from './app.component';
       { initialNavigation: 'enabledBlocking' },
     ),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: themeLoader,
+      deps: [VmwClarityThemeService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ module-federation.config.js
@@ -0,0 +1,64 @@
+// Shared Nx module configuration
+// see https://nx.dev/module-federation/micro-frontend-architecture#micro-frontend-architecture
+
+// --------------------------------------------------------------------------------------------
+// sharedPackages are shared as singletons between MFE apps.
+// All other npm packages and workspace libraries are not shared to minimize mismatch errors.
+// TODO: revisit this list!
+const sharedNpmPackages = new Set([
+  '@angular/animations',
+  '@angular/cdk',
+  '@angular/common',
+  '@angular/common/http',
+  '@angular/core',
+  '@angular/forms',
+  '@angular/platform-browser',
+  '@angular/router',
+  '@cds/core',
+  '@clr/angular',
+  '@clr/ui',
+  '@ngrx/component-store',
+  '@ngrx/effects',
+  '@ngrx/entity',
+  '@ngrx/router-store',
+  '@ngrx/store',
+  '@vmw/ngx-utils',
+  '@vmw/ngx-vip',
+  'lodash',
+  'ngx-infinite-scroll',
+  'ngx-pipes',
+  'parse-url',
+  'rxjs',
+]);
+
+const sharedLibraries = new Set([
+  '@seed/shared-models',
+  '@seed/shared-modules',
+  '@seed/shared-services',
+  '@seed/shared-styles',
+  '@seed/shared-ui',
+  '@seed/shared-utils',
+  '@seed/shared-vip',
+]);
+
+// --------------------------------------------------------------------------------------------
+
+module.exports = {
+  shared: (libraryName, defaultConfig) => {
+    if (sharedNpmPackages.has(libraryName)) {
+      // Nx defaultConfig is:
+      //  "singleton": true,
+      //  "strictVersion": true,
+      //  "requiredVersion": <version installed>
+      return defaultConfig;
+    } else if (sharedLibraries.has(libraryName)) {
+      return {
+        singleton: true,
+        requiredVersion: false,
+      };
+    }
+
+    // Returning false means the library is not shared.
+    return false;
+  },
+};
