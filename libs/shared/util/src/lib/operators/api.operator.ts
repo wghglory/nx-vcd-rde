9250e2498b7796adb5ceb7507d2920c14a19e20b
Derek Wang <guanghuiw@vmware.com>
2023-03-04 23:46:42 +0800

    feat: enhance api operator

--- libs/shared/util/src/lib/operators/api.operator.ts
+++ libs/shared/util/src/lib/operators/api.operator.ts
@@ -1,12 +1,40 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ApiQuery } from '@seed/shared/model';
-import { catchError, map, Observable, of, pipe, startWith, UnaryFunction } from 'rxjs';
+import { catchError, map, Observable, of, pipe, startWith, tap, UnaryFunction } from 'rxjs';
 
-export function api<T>(): UnaryFunction<Observable<T>, Observable<ApiQuery<T>>> {
+/**
+ * Custom operator for api call with loading, error, data.
+ *
+ * usage 1: Simple 1 request
+ * data$ = this.shopService.products$.pipe(
+    api(
+      res => console.log('side effect if success: ' + res),
+      error => {
+        console.error('side effect if error: ' + error.message);
+      },
+    ),
+
+    usage 2: dependent request
+    data$ = firstCall$.pipe(
+      switchMap(() => this.shopService.products$),
+      api(),
+    ),
+  );
+ * @param callback successful callback (side effect), e.g. close dialog
+ * @param errorCallback error callback, e.g. send toast alert
+ */
+export function api<T>(
+  callback?: (source?: T) => void,
+  errorCallback?: (error: HttpErrorResponse) => void,
+): UnaryFunction<Observable<T>, Observable<ApiQuery<T>>> {
   return pipe(
     map(data => ({ loading: false, error: null, data })),
+    tap({
+      next: res => callback?.(res.data),
+      error: err => errorCallback?.(err),
+    }),
     startWith({ loading: true, error: null, data: null }),
+    // retry(1), // if want to add retry
     catchError(error => of({ loading: false, error, data: null })),
   );
 }
-
-// Improvements: side effect after success or error
