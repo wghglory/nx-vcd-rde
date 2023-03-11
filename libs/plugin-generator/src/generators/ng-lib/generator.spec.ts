e8a9c0e7e25bcb82f8bcb29d0a23a634440ff7f1
Derek Wang <guanghuiw@vmware.com>
2023-03-11 15:42:44 +0800

    feat: create generator

---
+++ libs/plugin-generator/src/generators/ng-lib/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { NgLibGeneratorSchema } from './schema';
+
+describe('ng-lib generator', () => {
+  let appTree: Tree;
+  const options: NgLibGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace({layout: 'apps-libs'});
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ libs/plugin-generator/src/generators/ng-lib/schema.d.ts
@@ -0,0 +1,46 @@
+import { AngularGeneratorType } from '../generator.model';
+
+export interface NgLibGeneratorSchema {
+  type: AngularGeneratorType;
+  scope: GeneratorScope;
+  mfeName: string;
+  domain: string;
+
+  // angular defaults
+  name: string;
+  directory?: string;
+  addTailwind?: boolean;
+  skipFormat?: boolean;
+  simpleName?: boolean;
+  addModuleSpec?: boolean;
+  sourceDir?: string;
+  buildable?: boolean;
+  publishable?: boolean;
+  importPath?: string;
+  standaloneConfig?: boolean;
+  spec?: boolean;
+  flat?: boolean;
+  commonModule?: boolean;
+  prefix?: string;
+  routing?: boolean;
+  lazy?: boolean;
+  parent?: string;
+  tags?: string;
+  strict?: boolean;
+  linter?: Linter;
+  unitTestRunner?: UnitTestRunner;
+  compilationMode?: 'full' | 'partial';
+  setParserOptionsProject?: boolean;
+  skipModule?: boolean;
+  skipPackageJson?: boolean;
+  standalone?: boolean;
+  displayBlock?: boolean;
+  inlineStyle?: boolean;
+  inlineTemplate?: boolean;
+  viewEncapsulation?: 'Emulated' | 'None' | 'ShadowDom';
+  changeDetection?: 'Default' | 'OnPush';
+  style?: 'css' | 'scss' | 'sass' | 'less' | 'none';
+  skipTests?: boolean;
+  selector?: string;
+  skipSelector?: boolean;
+}
